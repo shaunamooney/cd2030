55bc12fc9e8b9e0cccf55d82026ce11223602528
shaunamooney <73603731+shaunamooney@users.noreply.github.com>
2025-04-03 13:52:13 +0100

    updating workflow script

--- script/cd_workflow.R
+++ script/cd_workflow.R
@@ -13,5 +13,27 @@ adjusted_data <- adjust_service_data(bf_data)
 # load in un estimates
 un_estimates_data <- load_un_estimates("~/private/Burkina Faso/UN_Estimates_Workshop.dta", country_iso = "BFA", start_year = 2019, end_year = 2023)
 
+# load the wuenic estimates
+wuenic_data <- load_wuenic_data('~/private/Burkina Faso/Wuenic_estimates.dta', country_iso = 'BFA')
+
+# load the national survey data and filter only survey greater than 2010
+survey_data <- load_survey_data('~/private/Burkina Faso/Survdata_Survdata_Burkina_Faso/all_Survdata_Burkina_Faso.dta', country_iso = 'BFA', admin_level = 'national') %>%
+  filter(year >= 2010)
+
 # calculate indicator coverage using adjusted data and un estimates at national level
-calculate_indicator_coverage(adjusted_data, admin_level = "national", un_estimates = un_estimates_data)
+#calculate_indicator_coverage(adjusted_data, admin_level = "national", un_estimates = un_estimates_data)
+
+# plot the indicator coverage for class 'cd_indicator_coverage'
+# plot(coverage, plot ='delivery_coverage_penta1')
+
+# calculate indicator coverage using adjusted data and un estimates, wuenic estimates and survey data at national level
+coverage <- calculate_coverage(adjusted_data,
+                               admin_level = "national",
+                               un_estimates = un_estimates_data,
+                               wuenic_data = wuenic_data,
+                               survey_data = survey_data)
+
+# plot the coverage for class 'cd_coverage'
+plot(coverage,
+     indicator = 'penta1',
+     denominator = 'penta1')
