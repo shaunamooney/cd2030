40ce23a19a0b9fff27d224fb176357c07b398287
shaunamooney <73603731+shaunamooney@users.noreply.github.com>
2025-05-14 21:59:58 +0100

    Update 1c_adjust_service_data_w_uncertainty.R

--- R/1c_adjust_service_data_w_uncertainty.R
+++ R/1c_adjust_service_data_w_uncertainty.R
@@ -6,7 +6,7 @@
 #' @param data A preprocessed dataframe that includes:
 #'   - Columns ending in `_rr` (reporting rates)
 #'   - Columns ending in `_k_value` (k-factor values per indicator)
-#'   - A logical column `impute_rr` to flag where reporting rates are to be imputed
+#'   - A logical column `impute_rr` to flag where reporting rates have been imputed
 #' @param n_samples Integer. Number of samples to draw per observation. Default is `500`.
 #'
 #' @export
@@ -43,8 +43,7 @@ adjust_with_uncertainty <- function(data, n_samples = 10){
     )
 
   adjusted_data_final <- adjusted_data_samples %>%
-    mutate(
-      across(
+    mutate(across(
         all_of(all_indicators),
         ~ {
           # Identify the main indicator group for the current sub-indicator
@@ -54,12 +53,12 @@ adjust_with_uncertainty <- function(data, n_samples = 10){
           rate <- get(paste0(group, '_rr_sample'))
 
           # Retrieve the k-value from the k_defaults list based on the group
-          k_value <- get(paste0(cur_column(), '_k_value_sample'))
+          k_value_sample <- get(paste0(cur_column(), '_k_value_sample'))
 
           # Apply the adjustment formula if the rate is not missing or zero
           if_else(
             !is.na(rate) & rate != 0,
-            round(. * (1 + (1 / (rate / 100) - 1) * k_value), 0),
+            . * (1 + (1 / (rate / 100) - 1) * k_value_sample), 0,
             .
           )
         }
@@ -68,13 +67,12 @@ adjust_with_uncertainty <- function(data, n_samples = 10){
   impute_data <- adjusted_data_final %>%
     add_outlier5std_column(all_indicators) %>%
     mutate(
-
       across(
         all_of(all_indicators),
         ~ {
           outlier <- get(paste0(cur_column(), '_outlier5std'))
           med <- round(median(if_else(outlier != 1, ., NA_real_), na.rm = TRUE), 0)
-          ifelse(outlier == 1, truncnorm::rtruncnorm(1, a = 0, mean = robust_max(med), sd = 50), .)
+          ifelse(outlier == 1, truncnorm::rtruncnorm(1, a = 0, mean = robust_max(med), sd = 100), .)
         }
       ),
 
@@ -84,7 +82,7 @@ adjust_with_uncertainty <- function(data, n_samples = 10){
           med <- round(median(if_else(!is.na(.), ., NA_real_), na.rm = TRUE), 0)
           max_med <- robust_max(med)
           if_else(
-            is.na(.) & !is.na(max_med), truncnorm::rtruncnorm(1, mean = max_med, a = 0, sd = 50),
+            is.na(.) & !is.na(max_med), truncnorm::rtruncnorm(1, mean = max_med, a = 0, sd = 100),
             .
           )
         }
@@ -94,10 +92,12 @@ adjust_with_uncertainty <- function(data, n_samples = 10){
     ) %>%
     select(-any_of(paste0(all_indicators, '_rr')))
 
-
   summary_data <- impute_data %>%
     group_by(country, adminlevel_1, district, month, year) %>%
     summarise(
+      anc_rr = first(anc_rr),
+      idelv_rr = first(idelv_rr),
+      vacc_rr = first(vacc_rr),
       across(
         all_of(all_indicators),
         list(
