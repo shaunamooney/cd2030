d8be2634ac7265332a0d091c3f3b332e1c8932e7
shaunamooney <73603731+shaunamooney@users.noreply.github.com>
2025-05-15 17:10:12 +0100

    updating workflow script

--- script/cd_workflow_w_uncertainty.R
+++ script/cd_workflow_w_uncertainty.R
@@ -5,19 +5,25 @@
 devtools::load_all()
 
 # load in bf data
-bf_data <- load_excel_data("~/private/Burkina Faso/Burkina_Faso.xlsx")
+bf_data <- load_excel_data("~/Documents/GitHub/cdroutine/private/Burkina Faso/Burkina_Faso.xlsx")
 
-# adjust the data include_uncertainty = TRUE uses updated functionality here - use k = 0.25 default as we don't know exact k values used
+# adjust the data include_uncertainty = TRUE uses updated functionality here
 adjusted_data <- adjust_service_data(bf_data, include_uncertainty = TRUE)
 
+# the following naming structure of columns applies to all indicators:
+adjusted_data$instlivebirths # this is the mean - named it the indicator to align with the rest of the code
+adjusted_data$instlivebirths_lower # 2.5th percentile
+adjusted_data$instlivebirths_upper # 97.5th percentile
+adjusted_data$instlivebirths_median # also added in the median for each indicator in the output
+
 # load in un estimates
-un_estimates_data <- load_un_estimates("~/private/Burkina Faso/UN_Estimates_Workshop.dta", country_iso = "BFA", start_year = 2019, end_year = 2023)
+un_estimates_data <- load_un_estimates("~/Documents/GitHub/cdroutine/private/Burkina Faso/UN_Estimates_Workshop.dta", country_iso = "BFA", start_year = 2019, end_year = 2023)
 
 # load the wuenic estimates
-wuenic_data <- load_wuenic_data('~/private/Burkina Faso/Wuenic_estimates.dta', country_iso = 'BFA')
+wuenic_data <- load_wuenic_data('~/Documents/GitHub/cdroutine/private/Burkina Faso/Wuenic_estimates.dta', country_iso = 'BFA')
 
 # load the national survey data and filter only survey greater than 2010
-survey_data <- load_survey_data('~/private/Burkina Faso/Survdata_Survdata_Burkina_Faso/all_Survdata_Burkina_Faso.dta', country_iso = 'BFA', admin_level = 'national') %>%
+survey_data <- load_survey_data('~/Documents/GitHub/cdroutine/private/Burkina Faso/Survdata_Burkina_Faso/all_Burkina_Faso.dta', country_iso = 'BFA', admin_level = 'national') %>%
   filter(year >= 2010)
 
 # calculate indicator coverage using adjusted data and un estimates at national level
